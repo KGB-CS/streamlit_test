4afb2ff4dc22f788479933b22cd1644c6b1399b4
n_nishimura <nishimura_n@chikumaseiki.co.jp>
2022-04-21 16:24:29 +0900

    HSV conversion

--- app.py
+++ app.py
@@ -4,19 +4,27 @@ import av
 import cv2
 
 st.title("Streamlit Test")
-st.write("Capture Image")
+st.write("HSV conversion")
 
 
 class VideoProcessor:
     def __init__(self) -> None:
-        self.threshold1 = 100
-        self.threshold2 = 200
+        self.Hue = 0
+        self.Sat = 100
+        self.Val = 100
 
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
 
-        img = cv2.cvtColor(cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR)
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) # 色空間をBGRからHSVに変換
+        h_deg = self.Hue        # 色相(Hue)の回転度数
+        s_mag = self.Sat / 100  # 彩度(Saturation)の倍率
+        v_mag = self.Val / 100  # 明度(Value)の倍率
 
+        img_hsv[:,:,(0)] = img_hsv[:,:,(0)] + h_deg     # 色相の計算
+        img_hsv[:,:,(1)] = img_hsv[:,:,(1)] * s_mag     # 彩度の計算
+        img_hsv[:,:,(2)] = img_hsv[:,:,(2)] * v_mag     # 明度の計算
+        img = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2BGR) # 色空間をHSVからBGRに変
         return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
@@ -28,5 +36,6 @@ ctx = webrtc_streamer(
     }
 )
 if ctx.video_processor:
-    ctx.video_processor.threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
-    ctx.video_processor.threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
+    ctx.video_processor.Hue = st.slider("Hue",          min_value=0, max_value=360, step=1, value=0)
+    ctx.video_processor.Sat = st.slider("Saturation",   min_value=0, max_value=100, step=1, value=100)
+    ctx.video_processor.Val = st.slider("Brightness",   min_value=0, max_value=100, step=1, value=100)    
